b8539d0beb98e0e2f6de6ee459eaab6177c999c6
Kostiantyn <kostiantyn@MBP-Kostiantyn.local>
2018-01-08 22:02:38 +0200

    init application

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
-const Koa = require('koa');
+import Koa from 'koa';
+// import config from  'config';
+// import err from './middleware/error';
+// import {routes, allowedMethods} from './middleware/routes';
 
 const app = new Koa();
 
-app.use(async ctx => {
-    ctx.body = 'Hello World';
-});
+global.console.log(app);
+// app.use(err);
+// app.use(routes());
+// app.use(allowedMethods());
 
-app.listen(3000);
+// app.listen(config.server.port, function () {
+//     console.log('%s listening at port %d', config.app.name, config.server.port);
+// });
